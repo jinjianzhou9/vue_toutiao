00446fbf8a7262cb1b2d237e7e89c5a0a1fad1c1
金建周 <2533151399@qq.com>
2021-12-05 17:50:01 +0800

    项目初始化-调整目录结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载全局样式文件
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建Vue根实例
 new Vue({
   router,
   render: h => h(App)
